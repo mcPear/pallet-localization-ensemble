1d1b56081113fac9b8fb5405a4259aa020c95171
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2020-04-04 20:52:31 +0200

    attach classifier to sliding window

--- dataset_io.py
+++ dataset_io.py
@@ -1,12 +1,13 @@
 from distutils.util import strtobool
 import os
+import cv2
+import numpy as np
 
 resolution_name_map = {"HD":(1280,720), "VGA":(640,480), "13MP":(4160,3120), "8MP":(3264,2448)}
 resolution_label_map = {"HD":(1280,720), "VGA":(1280, 960), "13MP":(1040,780), "8MP":(1040,780)} #this resolutions were used to make labelling easier
 DS_path = "/home/maciej/Desktop/pallet_dataset/"
 PROJECT_PATH = "/home/maciej/repos/pallet-recogntion-gpu/"
 SCENES_PATH = DS_path+"scenes/"
-IMG_SAFETY_BORDER=130
 CHANNELS_PATH=DS_path+"processed_scenes/channels/"
 
 def get_info_path(scene_name):
@@ -73,3 +74,33 @@ def get_processed_output_dir_path(output_dir_name):
 
 def get_scene_dir_path(scene):
     return DS_path+"scenes/"+scene+"/"
+
+def imread_resized(scene_name, filename, label_resolution):
+    filepath=get_filepath(scene_name, filename)
+    image = cv2.imread(filepath)
+    return cv2.resize(image, label_resolution)
+
+def read_split_channels(scene_name, filename):
+    filepath=get_precessed_filepath("channels", scene_name, filename)
+    ch14_path=filepath.replace(".jpg", "_ch14.png")
+    ch58_path=filepath.replace(".jpg", "_ch58.png")
+    ch14 = cv2.imread(ch14_path, cv2.IMREAD_UNCHANGED)
+    ch58 = cv2.imread(ch58_path, cv2.IMREAD_UNCHANGED)
+    return [ch14,ch58]
+
+def read_channels(scene_name, filename):
+    split_channels=read_split_channels(scene_name, filename)
+    return np.dstack(split_channels)
+
+def walk_dataset():
+    rows=[]
+    scene_names = get_scene_names()
+    for scene_name in scene_names:
+        include,_,_,_,label_resolution=read_info(scene_name)
+        if include:
+            filenames = get_image_names(scene_name)
+            labels_dict=read_labels_dict(scene_name)
+            for filename in filenames:
+                rects=labels_dict[filename]
+                rows.append((filename, scene_name, label_resolution, rects))
+    return rows
