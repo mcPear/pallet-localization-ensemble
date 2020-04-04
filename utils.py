06d63d68db191ee6bcd0a5a6062823ef5c41f951
Maciej Gruszczyński <maciek.gruszka@gmail.com>
2020-04-04 12:28:23 +0200

    structural classifier, refactoring in progress

--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
 from distutils.util import strtobool
+import os
 
 resolution_name_map = {"HD":(1280,720), "VGA":(640,480), "13MP":(4160,3120), "8MP":(3264,2448)}
 resolution_label_map = {"HD":(1280,720), "VGA":(1280, 960), "13MP":(1040,780), "8MP":(1040,780)} #this resolutions were used to make labelling easier
 DS_path = "/home/maciej/Desktop/pallet_dataset/"
-scenes_path = DS_path+"scenes/"
+PROJECT_PATH = "/home/maciej/repos/pallet-recogntion-gpu/"
+SCENES_PATH = DS_path+"scenes/"
 IMG_SAFETY_BORDER=130
+CHANNELS_PATH=DS_path+"processed_scenes/channels/"
 
-def read_info(scene_path):
+def get_info_path(scene_name):
+    return get_scene_path(scene_name)+"info.txt"
+
+def read_info(scene_name):
+    info_path=get_info_path(scene_name)
     result={}
-    with open(scene_path+"info.txt") as f:
+    with open(info_path) as f:
         for line in f:
            (key, val) = line.split()
            result[key] = val
@@ -36,6 +43,24 @@ def read_labels(scene):
         lines = [line.rstrip('\n') for line in f]
     lines = [parse_line(line) for line in lines]
     return lines
+
+def get_filepath(scene_name, filename):
+    return get_scene_path(scene_name)+filename
+
+def read_labels_dict(scene_name):
+    labels=read_labels(scene_name)
+    return {file : regions for file, regions in labels }
+
+def get_scene_names():
+    return sorted(os.listdir(SCENES_PATH))
+
+def get_image_names(scene_name):
+    scene_path=get_scene_path(scene_name)
+    names = sorted(os.listdir(scene_path))
+    return [f for f in names if f not in ["info.txt", "equalized", "denoised"]]
+
+def get_scene_path(scene_name):
+    return SCENES_PATH+scene_name+"/"
 
 def get_scene_labels_path(scene):
     return DS_path+"labels/"+scene+".txt"
